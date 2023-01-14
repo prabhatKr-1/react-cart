599d0f1c937f662144ef869ef2edf0975ec3dc5d
Prabhat Kumar <prabhatkumar.cse@gmail.com>
2023-01-15 02:50:42 +0530

    Updated implementation of the buttons using props

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,53 +1,20 @@
 import React from "react";
 
 class CartItem extends React.Component {
-  // Adding state
-  constructor() {
-    super();
-    this.state = {
-      title: "Phone",
-      price: 1000,
-      qty: 1,
-      img: "",
-    };
-  }
-  increaseQuantity = () => {
-    // Using setState to change the state, there are 2 ways to do this
-    // Form 1 -> Used when the prev state is not required
-    // this.setState({
-    //   qty: this.state.qty + 1,
-    // });
-    // Form 2 -> Used when the prev state is required
-    this.setState((prevState) => {
-      return {
-        qty: prevState.qty + 1,
-      };
-    });
-  };
-
-  decreaseQuantity = () => {
-    if (this.state.qty > 0) {
-      this.setState((prevState) => {
-        return {
-          qty: prevState.qty - 1,
-        };
-      });
-    }
-  };
-  delButton = () => {
-    this.setState({
-      qty: 0,
-    });
-  };
   render() {
-    // Restructuring so that we don't have to write this.state.XYX every time, instead we can just write XYZ
-    const { title, price, qty } = this.state;
+    // Destructuring so that we don't have to write this.state.XYX every time, instead we can just write XYZ
+    const { title, price, qty } = this.props.product;
+
+    // for the functionality of the buttons
+    const { product, onIncreaseQuantity, onDecreaseQuantity, onDeleteProduct } =
+      this.props;
 
     return (
       <div className="cart-item">
         <div className="left-block">
-          <img style={styles.image} alt =""/>
+          <img style={styles.image} alt="" />
         </div>
+
         <div className="right-block">
           {/* <div style={{ fontSize: 40, color: "red" }}>{this.state.title}</div> */}
           <div style={{ fontSize: 40, color: "red" }}>{title}</div>
@@ -55,25 +22,32 @@ class CartItem extends React.Component {
           <div style={{ color: "blue", fontSize: 30 }}>Rs {price}</div>
 
           <div style={{ color: "red", fontSize: 30 }}>Qty:{qty}</div>
+
           <div className="cart-item-action">
             {/* Buttons */}
             <img
               className="action-icons"
               src="https://cdn-icons-png.flaticon.com/512/1828/1828919.png"
               alt="plus-icon"
-              onClick={this.increaseQuantity}
+              // handling the click event
+              // here we haven't written this everwere coz we have already destructured it
+              onClick={() => onIncreaseQuantity(product)}
             />
+
             <img
               className="action-icons"
               src="https://cdn-icons-png.flaticon.com/512/1828/1828899.png"
               alt="minus-icon"
-              onClick={this.decreaseQuantity}
+              onClick={() => {
+                onDecreaseQuantity(product);
+              }}
             />
+
             <img
               className="action-icons"
               src="https://cdn-icons-png.flaticon.com/512/484/484611.png"
               alt="del-icon"
-              onClick={this.delButton}
+              onClick={() => onDeleteProduct(product.id)}
             />
           </div>
         </div>
@@ -82,13 +56,15 @@ class CartItem extends React.Component {
   }
 }
 
+// css style for the respective section
 const styles = {
   image: {
-    height: 300,
-    width: 300,
-    backgroundColor: "red",
-    borderRadius: 30,
+    height: 210,
+    width: 210,
+    backgroundColor: "#a28599",
+    borderRadius: "15%",
   },
 };
 
+// exporting so that it can be imported
 export default CartItem;
